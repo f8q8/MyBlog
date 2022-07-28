386e0bd729f189a7272f50ec9cb02337e1416d1e
zhaoxinle <zhaoxinle@tinypace.com>
2022-07-28 10:01:59 +0800

    增加部分配置，增加mongo地址

---
+++ src/main/java/com/f8q8/note/NoteApplication.java
@@ -0,0 +1,18 @@
+package com.f8q8.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhaoxinle
+ * @ClassName NodeApplication
+ * @description TODO
+ * @datetime 2022年 07月 28日 09:24:33
+ */
+@SpringBootApplication
+public class NoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteApplication.class, args);
+    }
+}
